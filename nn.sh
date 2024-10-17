47859cbd09aa839796bfc17a449f0cd1dda2fd2d
nitinbot001 <nitinbhujwa@gmail.com>
2024-10-17 15:11:50 +0530

    first commit

---
+++ nn.sh
@@ -0,0 +1,7 @@
+echo "# Nano" >> README.md
+git init
+git add *
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/NitinBot001/Nano.git
+git push -u origin main
